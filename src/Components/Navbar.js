f90354ebf598b2efbd042451061bd060ceb7aa9a
Soham Agarwal <121bm0729@nitrkl.ac.in>
2023-05-28 14:05:28 +0530

    Changed navbar color to red (danger)

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,10 @@ export class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav
+          className="navbar navbar-expand-lg bg-danger"
+          data-bs-theme="dark"
+        >
           <div className="container-fluid">
             <a className="navbar-brand" href="/">
               NewsMonkey
