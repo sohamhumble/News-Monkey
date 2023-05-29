4abab846ad420ebbce09b4dbaab4459a6721b95f
Soham Agarwal <121bm0729@nitrkl.ac.in>
2023-05-29 16:05:56 +0530

    Add news articles 	- Manually add news articles in codebase 	- set them using this.state 	- iterate them using map 	- display all news in bootstrap cards with title, desc, image and source link 	- add default image i case image from api is null Warning pending : 	- use of target="_blank" is will return a warning. 	- Should be solved using router in future versions.

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,17 +2,15 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
       <div>
-        <div className="card" style={{width: "18rem"}}>
-          <img src="https://static.vecteezy.com/system/resources/thumbnails/006/299/370/original/world-breaking-news-digital-earth-hud-rotating-globe-rotating-free-video.jpg" className="card-img-top" alt="..." />
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={imageUrl} className="card-img-top" alt="" />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="/" className="btn btn-primary">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
               Go somewhere
             </a>
           </div>
