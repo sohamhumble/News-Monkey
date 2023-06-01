cd8cd0100fc9103ccd94d6a35641af71933a29af
Soham Agarwal <121bm0729@nitrkl.ac.in>
2023-06-01 22:37:59 +0530

    Category setup using React Router     - Install React Router in the project     - Add Router to App.js for each category, unique key was provided to each Route for re-rendering of Components     - Each category in navbar is linked to its respective routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,106 @@ import "./App.css";
 import React, { Component } from "react";
 import Navbar from "./Components/Navbar";
 import News from "./Components/News";
+import {  Route, Routes, BrowserRouter } from "react-router-dom";
 
 export default class App extends Component {
   render() {
-    return <div>
-      <Navbar/>
-      <News pageSize={6} country="us" category="health"/>
-    </div>;
+    return (
+      <div>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <News
+                  key="general"
+                  pageSize={6}
+                  country="in"
+                  category="general"
+                />
+              }
+            ></Route>
+            <Route
+              path="/general"
+              element={
+                <News
+                  key="general"
+                  pageSize={6}
+                  country="in"
+                  category="general"
+                />
+              }
+            ></Route>
+            <Route
+              path="/business"
+              element={
+                <News
+                  key="business"
+                  pageSize={6}
+                  country="in"
+                  category="business"
+                />
+              }
+            ></Route>
+            <Route
+              path="/entertainment"
+              element={
+                <News
+                  key="entertainment"
+                  pageSize={6}
+                  country="in"
+                  category="entertainment"
+                />
+              }
+            ></Route>
+            <Route
+              path="/health"
+              element={
+                <News
+                  key="health"
+                  pageSize={6}
+                  country="in"
+                  category="health"
+                />
+              }
+            ></Route>
+            <Route
+              path="/science"
+              element={
+                <News
+                  key="science"
+                  pageSize={6}
+                  country="in"
+                  category="science"
+                />
+              }
+            ></Route>
+            <Route
+              path="/sports"
+              element={
+                <News
+                  key="sports"
+                  pageSize={6}
+                  country="in"
+                  category="sports"
+                />
+              }
+            ></Route>
+            <Route
+              path="/technology"
+              element={
+                <News
+                  key="technology"
+                  pageSize={6}
+                  country="in"
+                  category="technology"
+                />
+              }
+            ></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-
+import { Link } from "react-router-dom";
 export class Navbar extends Component {
   render() {
     return (
       <div>
         <nav className="navbar navbar-expand-lg bg-danger" data-bs-theme="dark">
           <div className="container-fluid">
-            <a className="navbar-brand" href="/">
+            <Link className="navbar-brand" to="/">
               NewsMonkey
-            </a>
+            </Link>
             <button
               className="navbar-toggler"
               type="button"
@@ -26,44 +26,39 @@ export class Navbar extends Component {
             >
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link" aria-current="page" href="/">
-                    Top Headlines
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    Business
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    Entertainment
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
+                  <Link className="nav-link" aria-current="page" to="/general">
                     General
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/">
+                  <Link className="nav-link" to="/business">
+                    Business
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/entertainment">
+                    Entertainment
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/health">
                     Health
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/">
+                  <Link className="nav-link" to="/science">
                     Science
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/">
+                  <Link className="nav-link" to="/sports">
                     Sports
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/">
+                  <Link className="nav-link" to="/technology">
                     Technology
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </div>
