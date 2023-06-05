c27cffbd964d6900f1dcd79354a29b1c5f555d92
Soham Agarwal <121bm0729@nitrkl.ac.in>
2023-06-05 16:16:22 +0530

    Improved source badge look

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -7,15 +7,11 @@ export class NewsItem extends Component {
     return (
       <div>
         <div className="card mb-3">
-          <span
-            className="position-absolute top-0 translate-middle badge rounded-pill bg-danger"
-            style={{
-              zIndex: "1",
-              left: "90%",
-            }}
-          >
-            {source}
-          </span>
+          <div style={{
+            position: 'absolute',
+          }}>
+            <span className="badge rounded-pill bg-danger">{source}</span>
+          </div>
           <img src={imageUrl} className="card-img-top" alt="" />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
